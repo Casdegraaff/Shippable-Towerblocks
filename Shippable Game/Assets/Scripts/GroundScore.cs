5d072a336780d65933d73b5549d62a4c72565def
Casdegraaff <54799365+Casdegraaff@users.noreply.github.com>
2020-03-10 11:30:42 +0100

    .

---
+++ Shippable Game/Assets/Scripts/GroundScore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GroundScore : MonoBehaviour
+{
+    public GameObject Floor;
+    public Text text;
+    public float ScoreTotal = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        void Total()
+        {
+            text.text = "Score: " + ScoreTotal;
+            if (Input.GetKeyDown(KeyCode.Mouse0) == true)
+            {
+                ScoreTotal += 1;
+
+            }
+        }
+        Total();
+    }
+}     
+
+
